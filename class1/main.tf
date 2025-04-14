045946709b5bf6b904ef2b32d6a3d3a0de32aedf
Aarvitexsathya <sreekarmani.ns@gmail.com>
2025-04-14 08:30:01 +0530

    alias update

--- class1/main.tf
+++ class1/main.tf
@@ -1,10 +1,16 @@
 provider "aws" {
   region = "us-east-1"
+  alias  = "North-Virginia"
+}
+provider "aws" {
+  region = "us-east-2"
+  alias  = "ohio"
 }
 
 resource "aws_instance" "Tf-class1" {
-  ami           = "ami-00a929b66ed6e0de6"
+  ami           = "ami-0100e595e1cc1ff7f"
   instance_type = "t2.micro"
+  provider = aws.ohio
   tags = {
     Name     = "Tf-class1"
     TeamName = "Devops"
